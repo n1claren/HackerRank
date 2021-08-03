83f3dacf1269d463c521ed3b15b55da078e6cb1a
Nikolay Ivanov <nikolay1892@abv.bg>
2021-08-03 21:44:43 +0300

    5th day of 10 days of js

---
+++ 10-Days-Of-JavaScript/Day-4/objects-problem.js
@@ -0,0 +1,26 @@
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    const rectangle = {
+        length: a,
+        width: b,
+        perimeter: 2*(a+b),
+        area: a*b
+    }
+    
+    return rectangle;
+}
+
+
+function main() {
+    const a = +(readLine());
+    const b = +(readLine());
+    
+    const rec = new Rectangle(a, b);
+    
+    console.log(rec.length);
+    console.log(rec.width);
+    console.log(rec.perimeter);
+    console.log(rec.area);
+}
