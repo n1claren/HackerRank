da377f1c7569334913d30f9d5ef52a8ea431034e
Nikolay Ivanov <nikolay1892@abv.bg>
2021-08-03 22:14:25 +0300

    8th day of 10 days of javascript

---
+++ 10-Days-Of-JavaScript/Day-7/regex-part-2.js
@@ -0,0 +1,21 @@
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
+     * followed by one or more letters.
+     */
+    
+    let re = /^(Mr|Mrs|Ms|Dr|Er)(\.)([a-zA-Z])+$/;
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    
+    console.log(!!s.match(re));
+}
